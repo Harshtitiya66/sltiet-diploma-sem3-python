edf5c82c2a58398173f5dbe63f06cc9174d07b32
Harsh Vyas <134424925+Harshtitiya66@users.noreply.github.com>
2023-09-18 12:55:20 +0530

    Create nasted_if.py

---
+++ nasted_if.py
@@ -0,0 +1,9 @@
+i = input("enter your age")
+name = input("enter your name")
+if (i < 18): 
+    print ("your not elgibal for voting") 
+    print ("plase come after 18 cur") 
+else: 
+    print ("i is greater than 15") 
+    print ("i'm in else Block") 
+print ("i'm not in if and not in else Block") 
